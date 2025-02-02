4d4e88ce9c9284867451d3636f09f12d3f5d8e33
Connor van Spronssen <info@dobefu.com>
2025-02-02 12:59:56 +0100

    Sun Feb  2 12:59:56 CET 2025

--- config/exports.zsh
+++ config/exports.zsh
@@ -22,7 +22,7 @@ export PATH=$PATH:$GOROOT/bin
 # Add fixed NVM version to the PATH. Important: The must be the first entry.
 export PATH="$HOME/.nvm/versions/node/v18.14.0/bin:$PATH"
 
-# Remove all special characters from the word chars
+# Remove all special characters from the word chars.
 export WORDCHARS=""
 
 # Set language variables.
@@ -31,6 +31,7 @@ export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 export LANGUAGE="en_US.UTF-8"
 
+# Set the TERMINFO path.
 export TERMINFO=/usr/lib/terminfo
 
 # Only enable fancy var_dumps on CLI.
@@ -45,9 +46,10 @@ export PASSWORD_STORE_GPG_OPTS='--no-throw-keyids'
 # Prevent Storybook from opening the browser.
 export BROWSER=none
 
-# Load NVM
+# Dynamically load NVM.
 NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 export NVM_DIR
+
 # shellcheck source=/dev/null
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use
 [[ -r $NVM_DIR/bash_completion ]] && source $NVM_DIR/bash_completion
