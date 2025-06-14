6c3055c3b05cfa420abe4a8c9eb75c5eb5726a41
Connor van Spronssen <info@dobefu.com>
2025-06-14 09:50:39 +0200

    Sat Jun 14 09:50:39 CEST 2025

--- config/completion.zsh
+++ config/completion.zsh
@@ -9,7 +9,7 @@ zstyle ':completion:*' group-name ''
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' menu select
 
-# Enable case insensitive matching, but only if there are no case-sensitive matches
+# Enable case insensitive matching, but only if there are no case-sensitive matches.
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 zstyle :compinstall filename '~/.zshrc'
