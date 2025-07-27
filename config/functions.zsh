1ccb87cdb49df27ee10cfb885fafd6eddf5f55da
Connor van Spronssen <info@dobefu.com>
2025-07-27 17:14:06 +0200

    Sun Jul 27 17:14:06 CEST 2025

--- config/functions.zsh
+++ config/functions.zsh
@@ -1,3 +1,5 @@
+# Custom exit function.
+# Prevents killing a Tmux session accidentally.
 function exit {
   if [ ${TMUX} ] && [ "$(tmux display-message -p '#S')" = 'remote' ] && [ "$(tmux list-panes | wc -l)" = 1 ] && [ "$(tmux list-windows | wc -l)" = 1 ]; then
     tmux detach
