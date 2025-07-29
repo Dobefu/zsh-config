4edaf4c8b6f1c90aa0ee14da7d0a9070bbe3ef9a
Connor van Spronssen <info@dobefu.com>
2025-07-29 08:00:35 +0200

    Tue Jul 29 08:00:35 CEST 2025

--- config/exports.zsh
+++ config/exports.zsh
@@ -27,6 +27,7 @@ export GOMODCACHE="$HOME/.cache/go-mod"
 # More MacOS overrides.
 if [[ "$(uname)" == "Darwin" ]]; then
   export GOROOT=/opt/homebrew/opt/go/libexec
+  export SDKROOT="$(xcrun --sdk macosx --show-sdk-path)"
 fi
 
 # Add fixed NVM version to the PATH.
